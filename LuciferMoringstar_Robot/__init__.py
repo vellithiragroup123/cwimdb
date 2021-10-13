555977cf9dc0b03c8d7bbacb45b13c1f211c4713
proffessorboy <91050350+proffessorboy@users.noreply.github.com>
2021-10-13 20:06:14 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,25 +11,7 @@ from .Channel import (
 )
 
 HELP = """
-<b><u>Basic Commads</u></b>
-
-➥ /start : check if am alive or dead
-➥ /about : About Me
-➥ /stats user Status
-
-<b><u>Bot Owner Only</u></b>
-
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
-
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
+**I cant help you**
 """
 
 ABOUT = """
@@ -37,11 +19,11 @@ ABOUT = """
     
 ➥ Credits : Subin
 
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
+➥ Editing : [Geronimo](t.me/geronimo1234)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [Click Me](https://t.me/)
 """
