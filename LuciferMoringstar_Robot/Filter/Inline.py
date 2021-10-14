8f4f633d54d1f1413541be615b6ecbaff597144d
vellithiragroup123 <88137446+vellithiragroup123@users.noreply.github.com>
2021-10-14 13:46:44 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,10 +89,10 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Creator', url='t.me/geronimo1234'),
+        InlineKeyboardButton('Maintainor', url='t.me/Oldmovinewmoviereliseupdates')
         ],[
-        InlineKeyboardButton('🖥️ Deploy Video 🖥️', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🎬Group🎬', url='https://t.me/joinchat/1Ew-KutgSx04YzI1')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
